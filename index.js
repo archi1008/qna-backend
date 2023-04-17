bc1f72e385f35818a0e2b0d46681da4cf341621b
archi1008 <gargarchi941@gmail.com>
2023-04-17 19:39:54 +0530

    node modules added

--- index.js
+++ index.js
@@ -51,5 +51,5 @@ return res.text;
 const PORT = process.env.PORT || 5000;
 console.log(PORT);
 app.listen(PORT, () => {
-    console.log('Server running on http://localhost:5000');
+    console.log(`Server running on ${PORT}`);
   });
